28b07558380d7da000d28e9750716801b915cbf1
Kirill Grouchnikov <kirillg@google.com>
2022-01-01 22:47:24 -0500

    Update copyright header year

--- src/main/kotlin/org/pushingpixels/radiance/ion/Conversion3.kt
+++ src/main/kotlin/org/pushingpixels/radiance/ion/Conversion3.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Ion Kirill Grouchnikov. All Rights Reserved.
+ * Copyright (c) 2021-2022 Ion Kirill Grouchnikov. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
