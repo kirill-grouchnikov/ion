a1c94e6dfb163da5d5fc63caf65465a658bccc83
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-01-14 12:56:25 -0500

    Upgrade dependencies
    Kotlin -> 2.3.0
    Radiance -> 8.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
     id("java")
 }
 
@@ -20,9 +20,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.pushing-pixels:radiance-animation:7.5.0")
-    implementation("org.pushing-pixels:radiance-animation-ktx:7.5.0")
+    implementation("org.pushing-pixels:radiance-animation:8.5.0")
+    implementation("org.pushing-pixels:radiance-animation-ktx:8.5.0")
 }
