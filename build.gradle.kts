78a445f345ef2867f1d92561c9022c5b29944121
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2026-06-03 15:55:59 -0400

    Upgrade dependencies
    Kotlin -> 2.4.0
    Gradle -> 9.5.1
    Coroutines -> 1.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.kotlin.jvm") version "2.4.0"
     id("java")
 }
 
@@ -20,9 +20,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     implementation("org.pushing-pixels:radiance-animation:8.5.0")
     implementation("org.pushing-pixels:radiance-animation-ktx:8.5.0")
 }
